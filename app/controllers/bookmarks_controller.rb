2a470167a8cc72cefde648603fa779d4aed9c90c
Carl Schierstaedt <carl.schierstaedt@gmail.com>
2021-11-12 12:01:27 +0100

    installed heroku

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,24 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(params_bookmark)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+ private
+
+  def params_bookmark
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  # get lists/:list_id/bookmarks/new
+  # @list = List.find(params list_id)
+  resources :bookmarks, only: [:destroy]
+  # get /bookmarks/:id
+  # @bookmark = Bookmark.find(params id)
 end
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
